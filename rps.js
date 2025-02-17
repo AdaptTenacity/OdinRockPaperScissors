57c6539239a8db3f6e6409ce057a7a6df065ab01
Sky Fox <181598130+AdaptTenacity@users.noreply.github.com>
2025-02-16 22:45:45 -0600

    various bug and formatting fixes.
    and I need to commit more often...

---
+++ rps.js
@@ -0,0 +1,64 @@
+// define array of options
+var rps = ["rock", "paper", "scissors"];
+
+//define function grabbing computor's random choice
+const getComputerChoice = () => {
+    return rps[Math.floor(Math.random() * rps.length)];
+};
+
+// Log choice to console.
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    const humanChoice = prompt("Let's play rock paper scissors!");
+console.log(humanChoice);
+return humanChoice;
+}
+// to get human choice, a human needs to choose rock, paper, or scissors. So those options have to be avaliable to them. 
+//the options must be selectable. The computer must intrepret the human decision and plug it back into the code for the next thing.
+
+// Tracking Scores
+
+var humansScore = 0
+var computerScore = 0
+
+// The game rounds
+
+function playRound(humanChoice, computerChoice) {
+        if (humanChoice ===  computerChoice) {
+        console.log("It is a tie!")
+
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")) {
+        console.log("You Win!")
+    } else 
+        console.log("You lose!")
+    
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
+
+// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+
+
+
+// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+// Increment the humanScore or computerScore variable based on the round winner.
+
+// Example code:
+
+// function playRound(humanChoice, computerChoice) {
+//   // your code here!
+// }
+
+// const humanChoice = getHumanChoice();
+// const computerChoice = getComputerChoice();
+
+// playRound(humanChoice, computerChoice);
